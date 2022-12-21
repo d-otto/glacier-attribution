5bc47476b438207840b1244bbda911a0f8f30f8a
Daniel Otto <drotto@uw.edu>
2022-12-20 20:12:21 -0800

    Switched to using code from esmvaltool for download.

--- src/data/download_cmip5.py
+++ src/data/download_cmip5.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-download_cmip5.py
+download_cmip6.py
 
 Description.
 
 Author: drotto
-Created: 12/14/2022 @ 11:21 AM
+Created: 12/15/2022 @ 11:25 AM
 Project: glacier-attribution
 """
 
@@ -13,93 +13,46 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+from pyesgf.search import SearchConnection
 from pathlib import Path
-
-from lib.cmip5_download.CEDA_download import *
-from config import ROOT
+from esmvalcore.esgf import find_files, download
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 #%%
 
-username, password = get_credentials()
+ensembles = [f"r{d}i1p1" for d in range(1, 100)]
+p1k_ensembles = [f"rii1p{d}" for d in [121, 122, 1221, 123, 124, 125, 126, 127, 128]]
+ensembles.extend(p1k_ensembles)
 
-group_models = [
-    ("BCC", "bcc-csm1-1"),
-    ("BCC", "bcc-csm1-1-m"),
-    ("BNU", "BNU-ESM"),
-    ("CCCma", "CanAM4"),
-    ("CCCma", "CanCM4"),
-    ("CCCma", "CanESM2"),
-    ("CMCC", "CMCC-CESM"),
-    ("CMCC", "CMCC-CM"),
-    ("CMCC", "CMCC-CMS"),
-    ("CNRM-CERFACS", "CNRM-CM5"),
-    ("CNRM-CERFACS", "CNRM-CM5-2"),
-    ("COLA-CFS", "CFSv2-2011"),
-    ("CSIRO-BOM", "ACCESS1-0"),
-    ("CSIRO-BOM", "ACCESS1-3"),
-    ("CSIRO-QCCCE", "CSIRO-Mk3-6-0"),
-    ("FIO", "FIO-ESM"),
-    ("ICHEC", "EC-EARTH"),
-    ("INM", "inmcm4"),
-    ("IPSL", "IPSL-CM5A-LR"),
-    ("IPSL", "IPSL-CM5A-MR"),
-    ("IPSL", "IPSL-CM5B-LR"),
-    ("LASG-CESS", "FGOALS-g2"),
-    ("LASG-IAP", "FGOALS-gl"),
-    ("LASG-IAP", "FGOALS-s2"),
-    ("MIROC", "MIROC-ESM"),
-    ("MIROC", "MIROC-ESM-CHEM"),
-    ("MIROC", "MIROC4h"),
-    ("MIROC", "MIROC5"),
-    ("MOHC", "HadGEM3"),
-    ("MOHC", "HadGEM2-A"),
-    ("MOHC", "HadGEM2-CC"),
-    ("MOHC", "HadGEM2-ES"),
-    ("MPI-M", "MPI-ESM-LR"),
-    ("MPI-M", "MPI-ESM-MR"),
-    ("MPI-M", "MPI-ESM-P"),
-    ("MRI", "MRI-AGCM3-2H"),
-    ("MRI", "MRI-AGCM3-2S"),
-    ("MRI", "MRI-CGCM3"),
-    ("MRI", "MRI-ESM1"),
-    ("NASA-GISS", "GISS-E2-H"),
-    ("NASA-GISS", "GISS-E2-H-CC"),
-    ("NASA-GISS", "GISS-E2-R"),
-    ("NASA-GISS", "GISS-E2-R-CC"),
-    ("NASA-GMAO", "GEOS-5"),
-    ("NCAR", "CCSM4"),
-    ("NCC", "NorESM1-M"),
-    ("NCC", "NorESM1-ME"),
-    ("NICAM", "NICAM-09"),
-    ("NIMR-KMA", "HADGEM2-AO"),
-    ("NOAA-GFDL", "GFDL-CM2p1"),
-    ("NOAA-GFDL", "GFDL-CM3"),
-    ("NOAA-GFDL", "GFDL-ESM2G"),
-    ("NOAA-GFDL", "GFDL-ESM2M"),
-    ("NOAA-GFDL", "GFDL-HIRAM-C180"),
-    ("NOAA-GFDL", "GFDL-HIRAM-C360"),
-    ("NOAA-NCEP", "CFSv2-2011"),
-    ("NSF-DOE-NCAR", "CESM1-BGC"),
-    ("NSF-DOE-NCAR", "CESM1-CAM5"),
-    ("NSF-DOE-NCAR", "CESM1-CAM5-1-FV2"),
-    ("NSF-DOE-NCAR", "CESM1-FASTCHEM"),
-    ("NSF-DOE-NCAR", "CESM1-WACCM"),
-]
+files = []
+fs = find_files(
+    project='CMIP5',
+    # output='output1',
+    mip='Amon',
+    short_name='tas',
+    dataset='*',
+    # dataset='MPI-ESM-P',
+    # exp=['historical', 'historicalNat', 'past1000', 'past2k'],
+    parent_experiment_id='past1000',
+    # start_year=['185001, 185101'],
+    # end_year=['200012']
+    # ensemble=ensembles[0],
+    # ensemble='r1i1p1',
+)
+files.extend(fs)
+logging.info('Search complete.')
 
-experiments = ["historical", "past1000", "historicalNat", ]
-freqs = ["mon", ]
-ensembles = ["r1i1p1", 'r1i1p121', 'r1i1p122', 'r1i1p1221', 'r1i1p123', 'r1i1p124', 'r1i1p125', 'r1i1p126', 'r1i1p127', 'r1i1p128']
+fs = find_files(
+    project='CMIP5',
+    mip='Amon',
+    short_name='tas',
+    dataset='*',
+    exp=['past1000', 'historical', 'historicalNat', 'past2k'],
+)
+files.extend(fs)
+logging.info('Search complete.')
 
-# Set 1 - cloud properties
-realms = ["atmos", ]
-out_realms = ["Amon", ]
-variables = ["tas"]
-
-logging.info("Set 1 - tas")
-datasets = get_datasets(group_models, experiments, freqs, realms,
-                        out_realms, ensembles, variables)
-download_batch(datasets, Path(ROOT, "data/external/gcm/cmip5"),
-               username, password, overwrite=False)
+dirname = Path(r'C:\sandbox\glacier-attribution\data\external\gcm\cmip5')
+download(files, dest_folder=dirname)
--- src/data/download_cmip6.py
+++ src/data/download_cmip6.py
@@ -14,22 +14,46 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from pyesgf.search import SearchConnection
+from pathlib import Path
+from esmvalcore.esgf import find_files, download
+import logging
 
-#%%
-conn = SearchConnection('https://esgf-node.llnl.gov/esg-search')
-ctx = conn.new_context(project='CMIP6', experiment='historical', variable='tas')
-print('Hits: {}, Realms: {}, Ensembles: {}'.format(
-    ctx.hit_count,
-    ctx.facet_counts['realm'],
-    ctx.facet_counts['ensemble']))
-print(ctx.get_facet_options())
+logging.basicConfig(level=logging.INFO)
 
 #%%
 
-conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True, timeout=240)
-ctx = conn.new_context(
+ensembles = [f"r{d}i1f1p1" for d in range(1, 200)]
+# p1k_ensembles = [f"rii1f1p1" for d in [121, 122, 1221, 123, 124, 125, 126, 127, 128]]
+# ensembles.extend(p1k_ensembles)
+
+files = find_files(
     project='CMIP6',
-    experiment='past1000',
-    facets='*',
-)
-ctx.hit_count
+    mip='Amon',
+    short_name='tas',
+    dataset='*',
+    exp=['historical', 'hist-nat', 'past1000', 'past2k'],
+    # ensemble=ensembles,
+)  
+
+dirname = Path(r'C:\sandbox\glacier-attribution\data\external\gcm\cmip6')
+download(files, dest_folder=dirname)
+
+#%%
+
+# conn = SearchConnection('https://esgf-node.llnl.gov/esg-search')
+# ctx = conn.new_context(project='CMIP6', experiment_id='past1000', variable='tas')
+# print('Hits: {}, Realms: {}, Ensembles: {}'.format(
+#     ctx.hit_count,
+#     ctx.facet_counts['realm'],
+#     ctx.facet_counts['ensemble']))
+# print(ctx.get_facet_options())
+# 
+# #%%
+# 
+# conn = SearchConnection('https://esgf-data.dkrz.de/esg-search', distrib=True, timeout=240)
+# ctx = conn.new_context(
+#     project='CMIP6',
+#     experiment='past1000',
+#     facets='*',
+# )
+# ctx.hit_count
