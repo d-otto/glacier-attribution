4b4f82bd32bf8739125b99c5b31e8b1ee31b6aff
Daniel Otto <drotto@uw.edu>
2022-12-20 20:18:38 -0800

    Fixed up for new data structure & speed improvements

--- src/data/concat_cmip_points.py
+++ src/data/concat_cmip_points.py
@@ -9,38 +9,51 @@ Created: 12/12/2022 @ 3:08 PM
 Project: glacier-attribution
 """
 
-import sys
-import os
-from pathlib import Path
-import pandas as pd
-import numpy as np
-import xarray as xr
+import logging
 import re
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import multiprocess as mp
 from itertools import product
+from pathlib import Path
 
-import src.data as data
+import multiprocess as mp
+import xarray as xr
+
 from config import ROOT
 
-#%%
+logging.basicConfig(level=logging.DEBUG)
 
-use_mp = True
-mip=6
-#processes = mp.cpu_count() - 2
-processes = 2
+# %%
 
-#%%
+use_mp = False
+mip = 5
+# processes = mp.cpu_count() - 2
+processes = 4
+
+
+# %%
+
 
 def mp_loop(rgiid):
-    pattern = f'^{rgiid}'
+    logging.debug(f"Working on {rgiid}")
+    pattern = f"^{rgiid}"
     fps = [fp for fp in ps if re.match(pattern, fp.name)]
-    identifiers = fps[0].name.split('_')[1:3]
-    ds = xr.open_mfdataset(fps, coords='minimal', data_vars='minimal')
+    identifiers = fps[0].name.split("_")[1:3]
+    ds = xr.open_mfdataset(
+        fps,
+        compat='no_conflicts',
+        engine='h5netcdf',
+        coords="minimal",
+        combine='nested',
+        concat_dim='time',
+        data_vars="minimal",
+        chunks=dict(variant=1, model=1),
+        parallel=True,
+        decode_cf=False,
+        use_cftime=True,
+    )
+    logging.debug("Successfully opened dataset.")
     p = Path(ROOT, f'features/gcm/cmip{mip}_{rgiid}_{"_".join(identifiers)}.nc')
-    ds.to_netcdf(p)
-    print(p.name)
+    ds.to_netcdf(p, format='netcdf4', engine='h5netcdf')
+
 
 # def mp_loop(rgiid):
 #     pattern = f'^{rgiid}'
@@ -59,28 +72,35 @@ def mp_loop(rgiid):
 #     print(p.name)
 
 
-#%%
-p = Path(ROOT, f'data/interim/gcm/cmip{mip}')
+# %%
+p = Path(ROOT, f"data/interim/gcm/cmip{mip}")
 ps = list(p.iterdir())
 fnames = [fp.name for fp in ps]
-rgiids = list(set([fname.split('_')[0] for fname in fnames]))
+rgiids = list(set([fname.split("_")[0] for fname in fnames]))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if use_mp:
         with mp.Pool(processes=processes) as pool:
             pool.starmap(mp_loop, product(rgiids))
-    else: 
+    else:
         for rgiid in rgiids:
             mp_loop(rgiid)
 
-
-
-#%% For debugging
-
+# %% For debugging
+# 
+# rgiid = rgiids[0]
 # concat = []
+# logging.debug(f"Working on {rgiid}")
+# pattern = f"^{rgiid}"
+# fps = [fp for fp in ps if re.match(pattern, fp.name)]
 # for fp in fps[0:10]:
-#     ds = xr.open_dataset(fp)
+#     ds = xr.open_dataset(fp,
+#         engine='h5netcdf',
+#         chunks=dict(variant=1, model=1),
+#         decode_cf=False,
+#         use_cftime=True,)
 #     concat.append(ds)
-#     
-# test = xr.combine_by_coords(concat)
-# test = xr.concat(concat, dim='time', coords='all')
+# 
+# test = xr.combine_by_coords(concat, compat='no_conflicts', coords="minimal",
+#         data_vars="minimal", combine_attrs='drop_conflicts')
+# # test = xr.concat(concat, dim='time', coords='all')
--- src/data/extract_cmip_points.py
+++ src/data/extract_cmip_points.py
@@ -14,13 +14,18 @@ import numpy as np
 import xarray as xr
 import netCDF4
 import h5netcdf
+import logging
 from pathlib import Path
 from tqdm import tqdm
 import multiprocess as mp
 from itertools import product
+import re
 
 from config import ROOT, cfg
 from src.data import get_cmip6, get_rgi, get_cmip6_run_attrs, extract_cmip_points, read_cmip_model, cmip_fname_to_dict
+from src import data
+
+logging.basicConfig(level=logging.DEBUG)
 
 # %%
 
@@ -33,19 +38,23 @@ rgi = rgi.set_index('RGIId')
 
 # %%
 
-use_mp = False
+use_mp = True
 use_cache = True
+
+# mip = 6
+# experiments = ['past1000', 'hist-nat', 'past2k', 'historical']
+
 mip = 6
-experiments = ['past1000', 'hist-nat', 'past2k', 'historical']
-# mip = 5
-# experiments = ['past1000', 'historical', 'historicalNat']
+experiments = ['past1000', 'historical', 'hist-nat', 'past2k']
+
 variant = None
 freq = 'jjas'
 variable = 'tas'
 
 def mp_loop(variable, fps, freq):
+    # variable, fps, freq = tup
     fp = fps[0]
-    segments = cmip_fname_to_dict(fp)
+    segments = data.cmip_fname_to_dict(fp)
     ps = [Path(ROOT, f"data/interim/gcm/cmip{mip}/{rgiid}_{variable}_{freq}_{segments['model']}_{segments['experiment']}_{segments['variant']}.nc")
         for rgiid in rgiids.values()]
     exists = [p.exists() for p in ps]
@@ -56,27 +65,20 @@ def mp_loop(variable, fps, freq):
             ds = read_cmip_model(fps=fps, freq=freq, mip=mip)
             extract_cmip_points(ds, glaciers=rgi, variable=variable, freq=freq, mip=mip)
             try:
-                print(f'Success! {fps[0]}')
+                logging.info(f'Success! {fps[0]}')
             except:
-                print(f'Success! {fps}')
+                logging.info(f'Success! {fps}')
         except:
-            print(f'Failed in the set including {fps[0]}')
+            logging.warning(f'Failed in the set including {fps[0]}')
     # ds = read_cmip_model(fps=fps, freq=freq, mip=mip)
     # extract_cmip_points(ds, glaciers=rgi, variable=variable, freq=freq)
     return None
 
 #%%
 if __name__ == '__main__':
-    
-    fps = []
     dirname = Path(ROOT, f'data/external/gcm/cmip{mip}')
-    for experiment in experiments:
-        models = get_cmip6_run_attrs(dirname=dirname, experiment=experiment, by='model')
-        for model in models:
-            variants = get_cmip6_run_attrs(dirname=dirname, experiment=experiment, model=model, by='variant')
-            for variant in variants:
-                fp = get_cmip6_run_attrs(dirname=dirname, experiment=experiment, model=model, variant=variant)
-                fps.append(fp)
+    fps = data.get_cmip_paths(dirname, mip=5, experiment=experiments, grouped=True)
+        
         
     if use_mp:
         with mp.Pool(processes=mp.cpu_count() - 2) as pool:
