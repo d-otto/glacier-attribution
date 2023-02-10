80e8380e6fe667dfdaa08ac84f431e2d433796c3
Daniel Otto <drotto@uw.edu>
2023-02-10 13:52:38 -0800

    Script to make catalog of downloaded files

--- src/data/get_netcdf_attrs.py
+++ src/data/get_netcdf_attrs.py
@@ -14,6 +14,7 @@ import numpy as np
 import netCDF4 as nc
 from pathlib import Path
 from tqdm import tqdm
+import re
 
 #%%
 concat = []
@@ -21,38 +22,51 @@ mips = [5, 6]
 for mip in mips:
     p = Path(f'C:\sandbox\glacier-attribution\data\external\gcm\cmip{mip}').glob('**/*.nc')
     ps = list(p)
-    lm = []
-    nat = []
-    anth = []
-    #pexp_ids = []
-    for fp in tqdm(ps):
+    concat = []
+    for fp in tqdm(ps, ascii=True):
         with nc.Dataset(fp, "r", format="NETCDF4") as ds:
             attrs = ds.__dict__.copy()
-            try:
-                # print(fp)
-                # print(ds.parent_experiment_id, ds.parent_experiment_rip)
-                # print(ds.experiment_id, ds.experiment, '\n')
-                # pexp_ids.append(ds.parent_experiment_id)
-                if (attrs.get('parent_experiment_id', False) not in ['esmControl', 'piControl']) or (attrs.get('experiment_id', False) in ['past1000', 'past2k']):
+
             
-                    attrs['collection'] = 'lm'
-                    lm.append(attrs)
-                elif (attrs.get('experiment_id') in ['hist-nat', 'historicalNat']):
             
+            if mip==6:
+                res = re.findall("([A-Za-z]+)(\d+)", attrs["variant_label"])
+                variant_components = dict(res)
+                
+                # sort into collections
+                if attrs["experiment_id"] == "hist-nat":
                     attrs['collection'] = 'nat'
-                    nat.append(attrs)
+                elif attrs["experiment_id"] in ["past1000", "past2k"]:
+                    attrs['collection'] = 'lm'
+                    
+                elif (attrs["experiment_id"] == "historical") & (
+                        variant_components["i"] in ["1000", "2000"]
+                ):
+                    attrs['collection'] = 'lm'
                 else:
-            
                     attrs['collection'] = 'anth'
-                    anth.append(attrs)
-            except:
-                raise BaseException
+                concat.append(attrs)
+                
+                    
+            elif mip==5:
+                # sort into collections
+                if attrs["experiment_id"] == "historicalNat":
+                    attrs['collection'] = 'nat'
+                elif attrs["experiment_id"] in ["past1000", "past2k"]:
+                    attrs['collection'] = 'lm'
+                else:
+                    attrs['collection'] = 'anth'
+                concat.append(attrs)
+            
        
     # pexp_ids = list(set(pexp_ids))
     # print(pexp_ids)
+    if mip == 5:
+        cols = ['model_id', 'experiment_id', 'table_id', 'initialization_method', 'physics_version', 'realization']
+    elif mip == 6:
+        cols = ['source_id', 'experiment_id', 'table_id', 'variable_id', 'variant_label']
     
-    df = pd.concat([pd.DataFrame(grp) for grp in [lm, nat, anth]], ignore_index=True)
-    attr_cols = [col for col in df.columns if col not in ['tracking_id', 'creation_date', 'history']]
-    df = df.drop_duplicates(subset=attr_cols)
+    df = pd.DataFrame(concat)
+    df = df.drop_duplicates(subset=cols, ignore_index=True)
     df.to_csv(Path(rf'C:\sandbox\glacier-attribution\data\external\gcm\cmip{mip}_catalog_raw.csv'), index=False)
 
